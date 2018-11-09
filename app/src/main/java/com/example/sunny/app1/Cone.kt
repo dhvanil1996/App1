ff88463ba8967d22dabf8f0f24505aea598814f5
dweaver3600 <dweaver3600@yahoo.com>
2018-11-08 20:04:02 -0800

    Add files via upload

---
+++ app/src/main/java/com/example/sunny/app1/Cone.kt
@@ -0,0 +1,138 @@
+package com.example.dweav.app1part2
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.widget.*
+import android.text.TextWatcher
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
+import kotlin.math.PI
+import kotlin.math.pow
+
+class ConeActivity : AppCompatActivity(), TextWatcher  { // change class name to the name of the calculator
+
+    var shapes = arrayOf("Cube", "Sphere", "Cone")
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+        setTitle("Surface Area of a Cone")
+
+        setContentView(R.layout.cone_activity)
+
+        var adapt = ArrayAdapter(this, android.R.layout.simple_spinner_item, shapes)
+
+        with(ShapesSpinner){
+            adapter = adapt
+            setSelection(2, false)
+
+        }
+
+        ShapesSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+                if (position == 0){
+                    startActivity( Intent(this@ConeActivity, MainActivity::class.java) )
+                }
+
+                if (position == 1){
+                    startActivity( Intent(this@ConeActivity, SphereActivity::class.java) )
+                }
+
+                else{}
+            }
+
+        }
+
+        val radiusEdit = findViewById<EditText>(R.id.RadiusEdit) as? EditText;
+        val heightEdit = findViewById<EditText>(R.id.HeightEdit) as? EditText;
+
+        val oneDecimal = DecimalFormat("#.#") // from https://www.programiz.com/kotlin-programming/examples/round-number-decimal
+
+        val answerView = findViewById<TextView>(R.id.AnswerView) as? TextView;
+
+        radiusEdit?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+                if (!radiusEdit?.text.isNullOrEmpty() && !heightEdit?.text.isNullOrEmpty()) {
+                    if (java.lang.Float.parseFloat(radiusEdit?.text.toString()) > 0) {
+
+                        val answer = PI * java.lang.Float.parseFloat(radiusEdit?.text.toString()) *
+                                (
+                                        java.lang.Float.parseFloat(radiusEdit?.text.toString())
+                                        +       java.lang.Float.parseFloat(radiusEdit?.text.toString()).pow(2)
+                                        +       java.lang.Float.parseFloat(heightEdit?.text.toString()).pow(2)
+                                        )
+                            //πr2h
+
+                        val answer_decimal = oneDecimal.format(answer);
+
+                        answerView?.setText(answer_decimal + " Area Cubed");
+
+                    }
+                } else
+                    answerView?.text = "0 Area Cubed"
+            }
+        })
+
+        heightEdit?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+                if (!radiusEdit?.text.isNullOrEmpty() && !heightEdit?.text.isNullOrEmpty()) {
+                    if (java.lang.Float.parseFloat(heightEdit?.text.toString()) > 0) {
+
+                        val answer = PI * java.lang.Float.parseFloat(radiusEdit?.text.toString()) *
+                                (
+                                        java.lang.Float.parseFloat(radiusEdit?.text.toString())
+                                                +       java.lang.Float.parseFloat(radiusEdit?.text.toString()).pow(2)
+                                                +       java.lang.Float.parseFloat(heightEdit?.text.toString()).pow(2)
+                                        )
+                        //A=πr(r+h2+r2)
+
+                        val answer_decimal = oneDecimal.format(answer);
+
+                        answerView?.setText(answer_decimal + " Area Cubed");
+
+                    }
+                } else
+                    answerView?.text = "0 Area Cubed"
+            }
+        })
+
+    } // end of override OnCreate
+
+
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+    }
+
+    override fun afterTextChanged(s: Editable) {
+
+    }
+}
--- app/src/main/java/com/example/sunny/app1/MainActivity.kt
+++ app/src/main/java/com/example/sunny/app1/MainActivity.kt
@@ -1,43 +1,150 @@
-package com.example.sunny.app1
+package com.example.dweav.app1part2
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.*
+import android.text.TextWatcher
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextWatcher  { // change class name to the name of the calculator
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    var shapes = arrayOf("Cube", "Sphere", "Cone")
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
+
+        setTitle("Surface Area of a Cube")
+
         setContentView(R.layout.activity_main)
+
+        var adapt = ArrayAdapter(this, android.R.layout.simple_spinner_item, shapes)
+
+        with(ShapesSpinner){
+            adapter = adapt
+            setSelection(0, false)
+
+        }
+
+        ShapesSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+                if (position == 1){
+                    startActivity( Intent(this@MainActivity, SphereActivity::class.java) )
+                }
+
+                if (position == 2){
+                    startActivity( Intent(this@MainActivity, ConeActivity::class.java) )
+                }
+
+                else{}
+            }
+
+        }
+
+        val lengthView = findViewById<EditText>(R.id.RadiusEdit) as? EditText;
+        val widthView = findViewById<EditText>(R.id.HeightEdit) as? EditText;
+        val heightView = findViewById<EditText>(R.id.HeightView) as? EditText;
+
+        val oneDecimal = DecimalFormat("#.#") // from https://www.programiz.com/kotlin-programming/examples/round-number-decimal
+
+        val answerView = findViewById<TextView>(R.id.AnswerView) as? TextView;
+
+        lengthView?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+                if (!lengthView?.text.isNullOrEmpty() && !widthView?.text.isNullOrEmpty() && !heightView?.text.isNullOrEmpty()) {
+                    if (Integer.parseInt(lengthView?.text.toString()) > 0) {
+
+                        val answer = java.lang.Float.parseFloat(lengthView.text.toString()) * java.lang.Float.parseFloat(widthView?.text.toString()) * java.lang.Float.parseFloat(heightView?.text.toString())
+
+                        val answer_decimal = oneDecimal.format(answer);
+
+                        answerView?.setText(answer_decimal + " Area Cubed");
+
+                    }
+                } else
+                    answerView?.text = "0 Area Cubed"
+            }
+        })
+
+        widthView?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+                if (!lengthView?.text.isNullOrEmpty() && !widthView?.text.isNullOrEmpty() && !heightView?.text.isNullOrEmpty()) {
+                    if (Integer.parseInt(lengthView?.text.toString()) > 0) {
+
+                        val answer = java.lang.Float.parseFloat(lengthView?.text.toString()) * java.lang.Float.parseFloat(widthView?.text.toString()) * java.lang.Float.parseFloat(heightView?.text.toString())
+
+                        val answer_decimal = oneDecimal.format(answer);
+
+                        answerView?.setText(answer_decimal + " Area Cubed");
+
+                    }
+                } else
+                    answerView?.text = "0 Area Cubed"
+            }
+        })
+
+        heightView?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+                if (!lengthView?.text.isNullOrEmpty() && !widthView?.text.isNullOrEmpty() && !heightView?.text.isNullOrEmpty()) {
+                    if (Integer.parseInt(lengthView?.text.toString()) > 0) {
+
+                        val answer = java.lang.Float.parseFloat(lengthView?.text.toString()) * java.lang.Float.parseFloat(widthView?.text.toString()) * java.lang.Float.parseFloat(heightView.text.toString())
+
+                        val answer_decimal = oneDecimal.format(answer);
+
+                        answerView?.setText(answer_decimal + " Area Cubed");
+
+                    }
+                } else
+                    answerView?.text = "0 Area Sq."
+            }
+        })
+    } // end of override OnCreate
+
+
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
     }
 
-    /** Called when user clicks Tips button **/
-    fun tipsActivity(view : View) {
-        val intent = Intent(this, Tips::class.java).apply{
-        }
-        startActivity(intent)
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
     }
 
-    /** Called when user clicks Calculator button **/
-    fun calcActivity(view: View){
-        val intent = Intent(this, Calculator::class.java).apply{
-        }
-        startActivity(intent)
-    }
+    override fun afterTextChanged(s: Editable) {
 
-
-    /** Called when user clicks Area button **/
-    fun areaActivity(view: View){
-        val intent = Intent(this, Area::class.java).apply{
-        }
-        startActivity(intent)
-    }
-
-    /** Called when user clicks Bmi button **/
-    fun bmiActivity(view: View){
-        val intent = Intent(this, BMI::class.java).apply{
-        }
-        startActivity(intent)
     }
 }
